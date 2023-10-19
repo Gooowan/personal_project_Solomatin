50f01bf72536624b02e6fd89363f28629bef0e8d
BUNJEE <vsolomatin@kse.org.ua>
2023-10-19 13:11:38 +0300

    Now everything pretty good, photo added, nav and screen resolutions working

---
+++ scripts/kebab_fetch.js
@@ -0,0 +1,21 @@
+// Fetch the kebab data from the provided kebabs.json file
+fetch('/data/kebabs.json')
+    .then(response => response.json())
+    .then(data => {
+        const kebabContainer = document.getElementById('kebab-container');
+
+        // Loop through each kebab data and create a div for it
+        data.forEach(kebab => {
+            const kebabDiv = document.createElement('div');
+            kebabDiv.classList.add('kebab-item');
+
+            kebabDiv.innerHTML = `
+                <h3>${kebab.name}</h3>
+                <img src="/images/${kebab.image}" alt="${kebab.name}">
+                <p>${kebab.description}</p>
+                <p class="cost">${kebab.price}</p>
+            `;
+
+            kebabContainer.appendChild(kebabDiv);
+        });
+    });
